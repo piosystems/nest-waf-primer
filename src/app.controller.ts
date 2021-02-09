52683a03667d235a8196ceb2934e7f347d83d209
Pius Onobhayedo <piosystems@yahoo.co.uk>
2021-02-09 12:18:44 +0100

    Post Render Implementation with Nunjucks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,28 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Render('home.html') //indicate the template to render
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+
+  @Get('about-us')
+  @Render('about-us.html') //indicate the template to render
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
+  
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  /**
+   * Here we will create another route relative to the controller's base root /
+   */
+  @Get('hello2')
+  @Render('index.html') //indicate the template to render
+  getHello2(): {} {
+    return this.appService.getHello2(); //the object returned will be passed to the template to render
   }
 }
