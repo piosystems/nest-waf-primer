2c86db37eb66d2a8178af58c9f0d7e5a37896c2b
Pius Onobhayedo <piosystems@yahoo.co.uk>
2021-02-10 20:25:49 +0100

    After implementing user create

---
+++ src/student-registration/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto {
+    
+    readonly firstName: string;
+    readonly middleName?: string;
+    readonly lastName: string;
+    readonly email: string;
+    readonly dateOfBirth: Date;
+    readonly nationality?: string
+    readonly address?: string
+    readonly isActive?: boolean;
+}
